b6ef09833cf997f966aa5ce07b2b96f78cc3a140
Viktoriia Cheriachukina <40663005@live.napier.ac.uk>
2025-10-10 19:26:30 +0100

    Updated Main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,47 +4,67 @@ import java.sql.*;
 
 public class App {
 
-    /** Соединение с MySQL */
+    /**
+     * Connection to MySQL database.
+     */
     private Connection con = null;
 
-    /** Подключение к БД с ретраями */
-    public void connect() {
-        try {
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // 60 попыток с паузой 5 сек (~5 минут)
-        for (int i = 1; i <= 60; i++) {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
-                con = DriverManager.getConnection(
-                        // если оба сервиса в docker-compose:
-                        "jdbc:mysql://db:3306/employees?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC",
-                        "root", "example"
-                );
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                return;
-            } catch (SQLException e) {
-                System.out.printf("Failed to connect attempt %d: %s (SQLState=%s, code=%d)%n",
-                        i, e.getMessage(), e.getSQLState(), e.getErrorCode());
-                try { Thread.sleep(5000); } catch (InterruptedException ignored) {}
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
             }
         }
-        System.out.println("Giving up: database not reachable.");
     }
 
-    /** Отключение от БД */
-    public void disconnect() {
-        if (con != null) {
-            try {
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
                 con.close();
-            } catch (SQLException e) {
+            }
+            catch (Exception e)
+            {
                 System.out.println("Error closing connection to database");
-            } finally {
-                con = null;
             }
         }
     }
@@ -56,8 +76,59 @@ public class App {
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
+    }
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
     }
 }
